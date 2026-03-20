1102aa8c1dd0071894738743c66bca833910658e
Kartik gupta <kartikgupta20042004@gmail.com>
2026-03-20 10:54:23 +0530

    Time: 94 ms (55.83%), Space: 134 MB (58.95%) - LeetHub

---
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.java
@@ -0,0 +1,62 @@
+class NumArray {
+    int[] tree;
+    int n;
+
+    void build(int[] arr, int node, int start, int end) {
+        if (start == end) {
+            tree[node] = arr[start];
+        } else {
+            int mid = (start + end) / 2;
+            build(arr, 2 * node + 1, start, mid);
+            build(arr, 2 * node + 2, mid + 1, end);
+            tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+        }
+    }
+
+    int query(int node, int start, int end, int l, int r) {
+        if (r < start || end < l)
+            return 0;
+        if (l <= start && end <= r)
+            return tree[node];
+        int mid = (start + end) / 2;
+        int left = query(2 * node + 1, start, mid, l, r);
+        int right = query(2 * node + 2, mid + 1, end, l, r);
+        return left + right;
+    }
+
+    void update(int node, int start, int end, int idx, int val) {
+        if (start == end) {
+            tree[node] = val;
+        } else {
+            int mid = (start + end) / 2;
+            if (idx <= mid)
+                update(2 * node + 1, start, mid, idx, val);
+            else
+                update(2 * node + 2, mid + 1, end, idx, val);
+            tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+        }
+    }
+    public NumArray(int[] nums) {
+        n = nums.length;
+        tree = new int[4 * n];
+        build(nums , 0 , 0 , n-1);
+        
+    }
+    
+    public void update(int index, int val) {
+        update(0 , 0 , n-1 , index , val);
+        
+    }
+    
+    public int sumRange(int left, int right) {
+        return query(0 , 0 , n-1 , left , right);
+
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(index,val);
+ * int param_2 = obj.sumRange(left,right);
+ */
