88171f69b3ee7fcd18ff6976a8cc8732610f54e0
Kartik gupta <kartikgupta20042004@gmail.com>
2026-04-08 15:18:17 +0530

    Time: 327 ms (6.76%), Space: 48.1 MB (13.67%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,10 +1,30 @@
 class Solution {
+    Boolean[] dp;
+    private boolean helper(int[] nums, int i){
+        if(i == nums.length-1) return true;
+        if(i>nums.length) return false;
+        if(dp[i] != null){
+            return dp[i];
+        }
+        for(int j=1; j<=nums[i]; j++){
+         if(helper(nums,i+j)) return dp[i] = true;
+        }
+        return dp[i] = false;
+    }
     public boolean canJump(int[] nums) {
-       int reachable = 0;
-       for(int i = 0; i < nums.length; i ++) {
-           if(i > reachable) return false;
-           reachable = Math.max(reachable, i + nums[i]);
-       } 
-       return true;
+        dp = new Boolean[nums.length];
+        return helper(nums,0);
     }
 }
+
+
+// class Solution {
+//     public boolean canJump(int[] nums) {
+//        int reachable = 0;
+//        for(int i = 0; i < nums.length; i ++) {
+//            if(i > reachable) return false;
+//            reachable = Math.max(reachable, i + nums[i]);
+//        } 
+//        return true;
+//     }
+// }
