6ff94e3e936b846fba18dfa672df92b864281cf8
Kartik gupta <kartikgupta20042004@gmail.com>
2026-05-11 11:54:04 +0530

    Time: 2 ms (83.17%), Space: 44.2 MB (12.5%) - LeetHub

--- 0920-number-of-music-playlists/0920-number-of-music-playlists.java
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.java
@@ -1,3 +1,34 @@
+class Solution {
+    public long helper(int count_song, int count_uniq, int goal, int n, int k, long[][] dp){
+        int mod = 1_000_000_007;
+        if(count_song==goal){
+            return (count_uniq==n) ? 1 : 0;
+        }
+        if(dp[count_song][count_uniq] != -1){
+            return dp[count_song][count_uniq];
+        }
+        long ans = 0;
+        // new song
+        if(count_uniq<n){
+           ans += helper(count_song+1,count_uniq+1,goal,n,k,dp)*(n-count_uniq) % mod;
+        }
+        // old songs
+        if(count_uniq>k){
+            ans += helper(count_song+1,count_uniq,goal,n,k,dp)*(count_uniq-k)% mod;
+        }
+       return dp[count_song][count_uniq] =(ans % mod);
+    }
+    public int numMusicPlaylists(int n, int goal, int k) {
+        long[][] dp = new long[goal+1][n+1];
+        for(int i=0; i<=goal; i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return (int)helper(0,0,goal,n,k,dp);
+    }
+}
+
+
+
 // class Solution {
 //     public int numMusicPlaylists(int n, int goal, int k) {
 //         int mod = 1000000007;
@@ -28,31 +59,3 @@
 // }
 
 
-class Solution {
-    public long helper(int i, int j, int goal, int n, int k, long[][] dp){
-        int mod = 1_000_000_007;
-        if(i==goal){
-            return (j==n) ? 1 : 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        long ans = 0;
-        // new song
-        if(j<n){
-           ans += helper(i+1,j+1,goal,n,k,dp)*(n-j) % mod;
-        }
-        // old songs
-        if(j>k){
-            ans += helper(i+1,j,goal,n,k,dp)*(j-k)% mod;
-        }
-       return dp[i][j] =(ans % mod);
-    }
-    public int numMusicPlaylists(int n, int goal, int k) {
-        long[][] dp = new long[goal+1][n+1];
-        for(int i=0; i<=goal; i++){
-            Arrays.fill(dp[i],-1);
-        }
-        return (int)helper(0,0,goal,n,k,dp);
-    }
-}
