50055eddfb99bbbc9ef14d641e781ac9dd6842c6
Kartik gupta <kartikgupta20042004@gmail.com>
2026-05-15 10:57:48 +0530

    Time: 37 ms (72.22%), Space: 46.3 MB (30.94%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,29 +1,48 @@
+// class Solution {
+//     int[] dp;
+//     public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+//         dp =  new int[n];
+//         Arrays.fill(dp , -1);
+//         int res = Integer.MIN_VALUE;
+
+//         for(int i = 0 ; i < n ; i++){
+//             res = Math.max(res , solve(nums , i, dp));
+//         }
+//         return res;
+//     }
+//     public int solve(int[] nums , int index , int dp[] ){
+//         if(index == nums.length){
+//             return 0;
+//         }
+//         if(dp[index] != -1) 
+//             return dp[index];
+
+//         int ans = 1;
+//         for(int j = 0 ; j < index ; j ++){
+//             if(nums[j] < nums[index]){
+//                 ans = Math.max(ans,(solve(nums,j, dp) +1));
+//             }
+//         }
+//         return dp[index] = ans;
+//     }
+// }
+
+
 class Solution {
-    int[] dp;
-    public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        dp =  new int[n];
-        Arrays.fill(dp , -1);
-        int res = Integer.MIN_VALUE;
-
-        for(int i = 0 ; i < n ; i++){
-            res = Math.max(res , solve(nums , i, dp));
-        }
-        return res;
-    }
-    public int solve(int[] nums , int index , int dp[] ){
-        if(index == nums.length){
-            return 0;
-        }
-        if(dp[index] != -1) 
-            return dp[index];
-
+    public int lengthOfLIS(int[] arr) {
+        int n = arr.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
         int ans = 1;
-        for(int j = 0 ; j < index ; j ++){
-            if(nums[j] < nums[index]){
-                ans = Math.max(ans,(solve(nums,j, dp) +1));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] < arr[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
             }
+            ans = Math.max(ans, dp[i]);
         }
-        return dp[index] = ans;
+        return ans;
     }
 }
