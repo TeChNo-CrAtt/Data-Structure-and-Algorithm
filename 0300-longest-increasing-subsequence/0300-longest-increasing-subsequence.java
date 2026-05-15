3fb470e49aef25a80ef5661c154f2bef05ecccee
Kartik gupta <kartikgupta20042004@gmail.com>
2026-05-15 10:05:15 +0530

    Time: 393 ms (7.99%), Space: 237.1 MB (11.91%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,27 +1,26 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[][] dp = new int[n][n+1];
+        int[][] dp = new int[n+1][n+1];
 
-        for(int[] row : dp){
+        for(int[] row : dp)
             Arrays.fill(row , -1);
-        }
-        return helper(nums, 0 , -1 , dp);
         
+        return solve(nums, 0 , -1 , dp);
     }
-    public int helper(int[] nums , int index , int prev , int[][] dp ){
-        if(index == nums.length)
+    public int solve(int[] nums , int index , int prev , int dp[][]){
+        if(index == nums.length){
             return 0;
-        
-        if(dp[index][prev+1] != -1)
-            return dp[index][prev+1];
-
-        int notTake = helper(nums , index + 1 , prev , dp );
-
-        int take = 0;
-        if(prev == -1 || nums[index] > nums[prev]){
-            take = 1 + helper(nums , index + 1 , index , dp);
         }
-        return dp[index][prev + 1] = Math.max(take , notTake);
+        if(dp[index][prev + 1] != -1) 
+            return dp[index][prev + 1];
+
+        int notpick = solve(nums , index + 1 , prev , dp);
+
+        int pick = 0;
+        if(prev == -1 || nums[index] > nums[prev]){
+            pick = 1 + solve(nums , index + 1 , index , dp);
+        }
+        return dp[index][prev + 1] = Math.max(pick , notpick);
     }
 }
