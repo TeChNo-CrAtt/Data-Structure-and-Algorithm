f47f4cb783fe7a40fe8d8383131784021f1d2620
Kartik gupta <kartikgupta20042004@gmail.com>
2026-05-19 12:26:28 +0530

    Time: 53 ms (40.98%), Space: 46.1 MB (94.04%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.java
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -1,38 +1,38 @@
 class Solution {
     public int longestStrChain(String[] words) {
-        int n = words.length;
         Arrays.sort(words, (a, b) -> a.length() - b.length());
+        int n=words.length;
         int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-        int ans = 1;
+        for (int i = 0; i < n; i++) {
+                dp[i] = 1;
+        }
+        int max=1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < i; j++) {
-                if (isPredecessor(words[j], words[i])) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                if (compare(words[i],words[j])&& 1+dp[j]>dp[i]) {
+                    dp[i] =  dp[j] + 1;
+                    max = Math.max(max, dp[i]);
                 }
             }
-
-            ans = Math.max(ans, dp[i]);
         }
-        return ans;
+        return max;
     }
-    public boolean isPredecessor(String s1, String s2) {
-        if (s1.length() + 1 != s2.length())
-            return false;
-
-        int i = 0;
-        int j = 0;
-
-        while (i < s1.length() && j < s2.length()) {
-
-            if (s1.charAt(i) == s2.charAt(j)) {
-                i++;
-                j++;
-            } else {
-                j++; 
+        public boolean compare(String s1,String s2){
+            if(s1.length()!=s2.length()+1){
+                return false;
             }
+            int first=0,second=0;
+            while(first<s1.length()){
+                if(second<s2.length() && s1.charAt(first)==s2.charAt(second)){
+                    first++;
+                    second++;
+                }else{
+                    first++;
+                }
+                
+            }
+            return second == s2.length();
         }
-
-        return i == s1.length();
-    }
+       
+    
 }
